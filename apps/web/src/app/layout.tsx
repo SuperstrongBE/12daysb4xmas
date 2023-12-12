b5fec08a163ef41d320f4fac1012b68eb5badd67
Remy Chauveau <remy@bequest.world>
2023-12-12 01:23:18 +0100

    WEB: Adding tailwind with daysiUI

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { StoreWrapper } from '@/components/04_biomes/StoreWrapper'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
