fe37839d1546cefb480eae85671a40f12438f3aa
Remy Chauveau <remy@bequest.world>
2023-12-13 17:56:39 +0100

    WEB: finalize details integration

--- apps/web/src/app/[templateId]/page.tsx
+++ apps/web/src/app/[templateId]/page.tsx
@@ -31,8 +31,9 @@ export default async function NFTPage({params}:NFTPageProps) {
       <div className="container grid justify-items-center grid-col-1 gap-8">
         <Image alt="Metallicus logo" src="/footer_brand_logo.png" width={130} height={34}></Image>
         <p className="text-center text-xs">
-        Metal Pay is a service of Metallicus, Inc., a licensed provider of money transfer services (NMLS ID: 2057807). All money transmission is provided by Metallicus, Inc. pursuant to Metallicus, Inc.’s licenses. © 2023 Metallicus, Inc. License issued to Metallicus by the Louisiana Office of Financial Institutions does not cover the exchange or transmission of virtual currency. All money transmission is provided by Metallicus, Inc. pursuant to Metallicus, Inc.'s licenses and/or the applicable law depending on the jurisdiction
-      </p>
+          Metal Pay is a service of Metallicus, Inc., a licensed provider of money transfer services (NMLS ID: 2057807).
+          All money transmission is provided by Metallicus, Inc. pursuant to Metallicus, Inc.’s licenses. © 2023 Metallicus, Inc. License issued to Metallicus by the Louisiana Office of Financial Institutions does not cover the exchange or transmission of virtual currency. All money transmission is provided by Metallicus, Inc. pursuant to Metallicus, Inc.'s licenses and/or the applicable law depending on the jurisdiction
+        </p>
       </div> 
     </main>
   )
